3f72298425b472562b4c41749c97386bf6ed0264
Ascariel <pablocangas@gmail.com>
2014-07-08 15:32:17 -0400

    testing checkout

---
+++ spec/support/todo_list_helper.rb
@@ -0,0 +1,8 @@
+module TodoListHelpers
+	def visit_todo_list(list)
+		visit "/todo_lists"
+		within dom_id_for(list)) do  #THIS DOM_ID_METHOD CAME FROM MY SUPPORT VIEW HELPERS
+			click_link("List Items")
+		end
+	end
+end
