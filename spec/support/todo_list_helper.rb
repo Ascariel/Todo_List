4668086898b07a6339014a1fca1f9dd8f57dfbab
Ascariel <pablocangas@gmail.com>
2014-07-08 15:41:09 -0400

    Rolled back to working version of app, after disaster

--- spec/support/todo_list_helper.rb
+++ spec/support/todo_list_helper.rb
@@ -1,7 +1,7 @@
 module TodoListHelpers
 	def visit_todo_list(list)
 		visit "/todo_lists"
-		within dom_id_for(list)) do  #THIS DOM_ID_METHOD CAME FROM MY SUPPORT VIEW HELPERS
+		within dom_id_for(list) do  #THIS DOM_ID_METHOD CAME FROM MY SUPPORT VIEW HELPERS
 			click_link("List Items")
 		end
 	end
