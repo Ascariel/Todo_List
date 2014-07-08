c2b41a37879f95539f7aae47622cba5e6931e9c8
Ascariel <pablocangas@gmail.com>
2014-07-08 11:27:51 -0400

    added spec helper module

--- spec/support/todo_list_helper.rb
+++ spec/support/todo_list_helper.rb
@@ -1,7 +1,7 @@
 module TodoListHelpers
 	def visit_todo_list(list)
 		visit "/todo_lists"
-		within("#todo_list_#{list.id}") do
+		within dom_id_for(list)) do  #THIS DOM_ID_METHOD CAME FROM MY SUPPORT VIEW HELPERS
 			click_link("List Items")
 		end
 	end
